eabf610f525a0dec16dfa91cbaa312ff58771052
ahamedajas <ajas.greenrose@gmail.com>
2023-07-30 10:58:06 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// JavaScript function to play audio
+function playAudio(audioSrc) {
+    const audio = new Audio(audioSrc);
+    const playingAudios = document.querySelectorAll('audio');
+
+    // Pause all other playing audios
+    playingAudios.forEach((playingAudio) => {
+        if (!playingAudio.paused && playingAudio !== audio) {
+            playingAudio.pause();
+        }
+    });
+
+    // Play or pause the selected audio
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
